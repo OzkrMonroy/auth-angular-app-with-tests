de423699eb0820a6056a3b7a399eb21e4cd08b57
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-16 05:31:17 -0600

    The test for the button in Dashboard has been implemented and passed.

--- src/app/protected/dashboard/dashboard.component.spec.ts
+++ src/app/protected/dashboard/dashboard.component.spec.ts
@@ -1,13 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { AuthService } from '../../auth/services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-xdescribe('DashboardComponent', () => {
+describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [AuthService],
       declarations: [DashboardComponent],
     }).compileComponents();
   });
@@ -20,5 +29,25 @@ xdescribe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to login when logout method is called', () => {
+    const router = TestBed.inject(Router);
+    const service = TestBed.inject(AuthService);
+
+    const spyService = spyOn(service, 'logout').and.callFake(() => of(null));
+    const routerSpy = spyOn(router, 'navigate');
+
+    component.logout();
+    expect(spyService).toHaveBeenCalledTimes(1);
+    expect(routerSpy).toHaveBeenCalledOnceWith(['/auth/login']);
+  });
+
+  it('Button should call the logout method when it is clicked', () => {
+    const spyLogout = spyOn(component, 'logout').and.callFake(() => null);
+    const button: DebugElement = fixture.debugElement.query(By.css('button'));
+
+    button.triggerEventHandler('click', null);
+    expect(spyLogout).toHaveBeenCalledTimes(1);
   });
 });
